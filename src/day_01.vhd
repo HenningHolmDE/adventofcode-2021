192871079870e80a184fa3c2728aefb9580eef2d
Henning Holm <git@henningholm.de>
2021-12-05 11:48:25 +0100

    Solution for part two of day 1

--- src/day_01.vhd
+++ src/day_01.vhd
@@ -22,7 +22,8 @@ architecture simulation of day_01 is
         263
     );
 
-    function number_of_increases(measurements : integer_vector) return natural is
+    -- return number of measurements that are higher than the previous one
+    function number_of_increases_part1(measurements : integer_vector) return natural is
 
         variable result : natural := 0;
     begin
@@ -33,6 +34,28 @@ architecture simulation of day_01 is
                     result := result + 1;
                 end if;
             end if;
+        end loop;
+        return result;
+    end function;
+
+    -- return number of three-measurement-sums that are higher than the previous one
+    function number_of_increases_part2(measurements : integer_vector) return natural is
+
+        constant WINDOW_SIZE       : natural := 3;
+        constant NUMBER_OF_WINDOWS : natural := measurements'length - (WINDOW_SIZE - 1);
+        variable window_sum        : natural;
+        variable last_window_sum   : natural;
+        variable result            : natural := 0;
+    begin
+        for I in 0 to NUMBER_OF_WINDOWS - 1 loop
+            window_sum := 0;
+            for J in I to I + WINDOW_SIZE - 1 loop
+                window_sum := window_sum + measurements(J);
+            end loop;
+            if I > 0 and window_sum > last_window_sum then
+                result := result + 1;
+            end if;
+            last_window_sum := window_sum;
         end loop;
         return result;
     end function;
@@ -48,27 +71,41 @@ begin
         variable ptr_values     : ptr_values_t;
         variable ptr_values_old : ptr_values_t;
     begin
-        result := number_of_increases(EXAMPLE_MEASUREMENTS);
-        report "Number of increases in example: " & integer'image(result);
-        assert result = 7;
-
+        -- load input values from file
         file_open(i_file, "inputs/day_01.txt", read_mode);
         while not endfile(i_file) loop
             readline(i_file, i_line);
             read(i_line, i_value);
             if ptr_values = null then
                 ptr_values := new integer_vector'(0 => i_value);
-                else
+            else
                 ptr_values_old := ptr_values;
                 ptr_values     := new integer_vector'(ptr_values.all & i_value);
                 deallocate(ptr_values_old);
             end if;
         end loop;
         file_close(i_file);
-
         report "Number of values in input: " & integer'image(ptr_values.all'length);
-        result := number_of_increases(ptr_values.all);
-        report "Number of increases in input (solution for part one): " & integer'image(result);
+
+        report "*** Part One ***";
+
+        result := number_of_increases_part1(EXAMPLE_MEASUREMENTS);
+        report "Number of increases in example: " & integer'image(result);
+        assert result = 7;
+
+        result := number_of_increases_part1(ptr_values.all);
+        report "Number of increases in input: " & integer'image(result);
+
+        report "*** Part Two ***";
+
+        result := number_of_increases_part2(EXAMPLE_MEASUREMENTS);
+        report "Number of increases in example: " & integer'image(result);
+        assert result = 5;
+
+        result := number_of_increases_part2(ptr_values.all);
+        report "Number of increases in input: " & integer'image(result);
+
+        deallocate(ptr_values);
         wait;
     end process;
 
