7c4c2fb5df011b0a95f528fdd788a6f646acc8ab
Henning Holm <git@henningholm.de>
2021-12-11 18:47:05 +0100

    Solution for day 6, part two

--- src/day_06.vhd
+++ src/day_06.vhd
@@ -5,12 +5,20 @@ end entity;
 
 use std.textio.all;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 architecture simulation of day_06 is
 
     constant EXAMPLE_INPUT : integer_vector := (3, 4, 3, 1, 2);
 
+    -- Use 64 bit unsigned as 32 bit natural isn't large enough for this challenge.
+    subtype big_unsigned_t is unsigned(63 downto 0);
+
     -- Type for holding fish population sorted by timer value.
-    subtype lanternfish_timers_t is integer_vector(0 to 8);
+    type big_unsigned_vector_t is array(integer range <>) of big_unsigned_t;
+    subtype lanternfish_timers_t is big_unsigned_vector_t(0 to 8);
 
     -- Simulate lanternfish growth of one day.
     function run_simulation_step(
@@ -28,23 +36,43 @@ architecture simulation of day_06 is
         return result;
     end function;
 
+    -- Simulate lanternfish growth over a given amount of days.
+    function run_simulation_steps(
+        fish     : lanternfish_timers_t;
+        num_days : positive
+    ) return lanternfish_timers_t is
+        variable result : lanternfish_timers_t;
+    begin
+        result := fish;
+        for DAY in 1 to num_days loop
+            result := run_simulation_step(result);
+        end loop;
+        return result;
+    end function;
+
     -- Simulate lanternfish growth for 80 days.
     function simulate_fish_growth_part1(
         fish : lanternfish_timers_t
     ) return lanternfish_timers_t is
         variable result : lanternfish_timers_t;
     begin
-        result := fish;
-        for DAY in 1 to 80 loop
-            result := run_simulation_step(result);
-        end loop;
-        return result;
+        return run_simulation_steps(fish, 80);
+    end function;
+
+    -- Simulate lanternfish growth for 256 days.
+    function simulate_fish_growth_part2(
+        fish : lanternfish_timers_t
+    ) return lanternfish_timers_t is
+        variable result : lanternfish_timers_t;
+    begin
+        return run_simulation_steps(fish, 256);
     end function;
 
     -- Sum fish over all timer values
-    function sum(fish : lanternfish_timers_t) return natural is
-        variable result   : natural := 0;
+    function sum(fish : lanternfish_timers_t) return unsigned is
+        variable result   : big_unsigned_t;
     begin
+        result := (others => '0');
         for I in fish'range loop
             result := result + fish(I);
         end loop;
@@ -62,17 +90,17 @@ begin
 
         variable example_fish : lanternfish_timers_t;
         variable input_fish   : lanternfish_timers_t;
-        variable result       : natural;
+        variable result       : big_unsigned_t;
     begin
         -- Load fish from example input
-        example_fish := (others => 0);
+        example_fish := (others => (others => '0'));
         for I in EXAMPLE_INPUT'range loop
             example_fish(EXAMPLE_INPUT(I)) := example_fish(EXAMPLE_INPUT(I)) + 1;
         end loop;
-        report "Number of fish in example input: " & integer'image(sum(example_fish));
+        report "Number of fish in example input: " & integer'image(to_integer(sum(example_fish)));
 
         -- Load fish from input file
-        input_fish := (others => 0);
+        input_fish := (others => (others => '0'));
         file_open(i_file, "inputs/day_06.txt", read_mode);
         readline(i_file, i_line);
         good := true;
@@ -82,19 +110,27 @@ begin
             read(i_line, char, good); -- skip ',' if available
         end loop;
         file_close(i_file);
-        report "Number of fish in input file: " & integer'image(sum(input_fish));
+        report "Number of fish in input file: " & integer'image(to_integer(sum(input_fish)));
 
         report "*** Part One ***";
 
         result := sum(simulate_fish_growth_part1(example_fish));
-        report "Number of fish after simulating example input: " & integer'image(result);
+        report "Number of fish after simulating example input: " & integer'image(to_integer(result));
         assert result = 5934;
 
         result := sum(simulate_fish_growth_part1(input_fish));
-        report "Number of fish after simulating input file: " & integer'image(result);
+        report "Number of fish after simulating input file: " & integer'image(to_integer(result));
         assert result = 380243;
 
-        -- report "*** Part Two ***";
+        report "*** Part Two ***";
+
+        result := sum(simulate_fish_growth_part2(example_fish));
+        report "Number of fish after simulating example input: 0x" & to_hstring(result);
+        assert result = d"26984457539";
+
+        result := sum(simulate_fish_growth_part2(input_fish));
+        report "Number of fish after simulating input file: 0x" & to_hstring(result);
+        assert result = d"1708791884591";
 
         wait;
     end process;
