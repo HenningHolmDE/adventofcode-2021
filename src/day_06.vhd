0246cfb6444497090263d5bf40bc1ade54132d78
Henning Holm <git@henningholm.de>
2021-12-11 18:30:04 +0100

    Refactor solution for day 6
    By sorting fish by timer value this works without any dynamic memory
    allocation and might scale for part two.

--- src/day_06.vhd
+++ src/day_06.vhd
@@ -9,94 +9,93 @@ architecture simulation of day_06 is
 
     constant EXAMPLE_INPUT : integer_vector := (3, 4, 3, 1, 2);
 
-    type integer_vector_ptr_t is access integer_vector;
+    -- Type for holding fish population sorted by timer value.
+    subtype lanternfish_timers_t is integer_vector(0 to 8);
 
     -- Simulate lanternfish growth of one day.
-    procedure run_simulation_step(
-        variable fish_ptr : inout integer_vector_ptr_t
-    ) is
-        variable number_of_new_fish : natural;
-        variable fish_ptr_old       : integer_vector_ptr_t;
+    function run_simulation_step(
+        fish : lanternfish_timers_t
+    ) return lanternfish_timers_t is
+        variable result : lanternfish_timers_t;
     begin
-        -- Decrease fish timers and count the number of new fish to be created.
-        number_of_new_fish := 0;
-        for I in fish_ptr'range loop
-            if fish_ptr(I) = 0 then
-                number_of_new_fish := number_of_new_fish + 1;
-                fish_ptr(I)        := 6;
-            else
-                fish_ptr(I) := fish_ptr(I) - 1;
-            end if;
-        end loop;
-        -- Create new fish if required.
-        if number_of_new_fish > 0 then
-            fish_ptr_old := fish_ptr;
-            fish_ptr     := new integer_vector(0 to fish_ptr'length + number_of_new_fish - 1);
-            fish_ptr.all := fish_ptr_old.all & (0 to number_of_new_fish - 1 => 8);
-            deallocate(fish_ptr_old);
-        end if;
-    end procedure;
+        -- decrement lanternfish timers
+        result := (
+            0 to 5 => fish(1 to 6),
+            6      => fish(7) + fish(0), -- decremented from 7 or restarted from 0
+            7      => fish(8),
+            8      => fish(0) -- new fish created by expired timers
+            );
+        return result;
+    end function;
 
     -- Simulate lanternfish growth for 80 days.
-    procedure simulate_fish_growth_part1(
-        variable fish_ptr : inout integer_vector_ptr_t
-    ) is
+    function simulate_fish_growth_part1(
+        fish : lanternfish_timers_t
+    ) return lanternfish_timers_t is
+        variable result : lanternfish_timers_t;
     begin
+        result := fish;
         for DAY in 1 to 80 loop
-            run_simulation_step(fish_ptr);
+            result := run_simulation_step(result);
         end loop;
-    end procedure;
+        return result;
+    end function;
+
+    -- Sum fish over all timer values
+    function sum(fish : lanternfish_timers_t) return natural is
+        variable result   : natural := 0;
+    begin
+        for I in fish'range loop
+            result := result + fish(I);
+        end loop;
+        return result;
+    end function;
 
 begin
 
     process
-        file i_file     : text;
-        variable i_line : line;
+        file i_file          : text;
+        variable i_line      : line;
+        variable timer_value : natural;
+        variable char        : character;
+        variable good        : boolean;
 
-        variable input_fish_ptr : integer_vector_ptr_t;
-        variable number_of_fish : natural;
-        variable char           : character;
-        variable good           : boolean;
-
-        variable fish_ptr : integer_vector_ptr_t;
+        variable example_fish : lanternfish_timers_t;
+        variable input_fish   : lanternfish_timers_t;
+        variable result       : natural;
     begin
-        -- Load lines from input file
+        -- Load fish from example input
+        example_fish := (others => 0);
+        for I in EXAMPLE_INPUT'range loop
+            example_fish(EXAMPLE_INPUT(I)) := example_fish(EXAMPLE_INPUT(I)) + 1;
+        end loop;
+        report "Number of fish in example input: " & integer'image(sum(example_fish));
+
+        -- Load fish from input file
+        input_fish := (others => 0);
         file_open(i_file, "inputs/day_06.txt", read_mode);
         readline(i_file, i_line);
-        number_of_fish := 1;
-        -- Add number of commas in line to find out number of fish
-        for I in i_line'range loop
-            if i_line(I) = ',' then
-                number_of_fish := number_of_fish + 1;
-            end if;
-        end loop;
-        -- Create input fish vector
-        input_fish_ptr := new integer_vector(0 to number_of_fish - 1);
-        for I in input_fish_ptr'range loop
-            read(i_line, input_fish_ptr(I));
-            read(i_line, char, good); -- skip ','
+        good := true;
+        while good loop
+            read(i_line, timer_value);
+            input_fish(timer_value) := input_fish(timer_value) + 1;
+            read(i_line, char, good); -- skip ',' if available
         end loop;
         file_close(i_file);
-        report "Number of fish in example input: " & integer'image(EXAMPLE_INPUT'length);
-        report "Number of fish in input file: " & integer'image(input_fish_ptr'length);
+        report "Number of fish in input file: " & integer'image(sum(input_fish));
 
         report "*** Part One ***";
 
-        fish_ptr := new integer_vector'(EXAMPLE_INPUT);
-        simulate_fish_growth_part1(fish_ptr);
-        report "Number of fish after simulating example input: " & integer'image(fish_ptr'length);
-        assert fish_ptr'length = 5934;
-        deallocate(fish_ptr);
+        result := sum(simulate_fish_growth_part1(example_fish));
+        report "Number of fish after simulating example input: " & integer'image(result);
+        assert result = 5934;
 
-        fish_ptr := new integer_vector'(input_fish_ptr.all);
-        simulate_fish_growth_part1(fish_ptr);
-        report "Number of fish after simulating input file: " & integer'image(fish_ptr'length);
-        assert fish_ptr'length = 380243;
-        deallocate(fish_ptr);
+        result := sum(simulate_fish_growth_part1(input_fish));
+        report "Number of fish after simulating input file: " & integer'image(result);
+        assert result = 380243;
 
         -- report "*** Part Two ***";
 
-        deallocate(input_fish_ptr);
         wait;
     end process;
 
