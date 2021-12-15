3a79d4ce4cbead2653f93dad16ec6fc26e8b97fb
Henning Holm <git@henningholm.de>
2021-12-15 16:58:44 +0100

    Solution for day 9, part two

--- src/day_09.vhd
+++ src/day_09.vhd
@@ -65,6 +65,110 @@ architecture simulation of day_09 is
         return result;
     end function;
 
+    -- Types used by flooding algorithm
+    type flooding_map_t is array(natural range <>, natural range <>) of boolean;
+    type flooding_state_t is record
+        flooding_map  : flooding_map_t;
+        flooding_size : natural;
+    end record;
+
+    -- Recursive flooding algorithm
+    function perform_flooding(
+        heightmap      : heightmap_t;
+        flooding_state : flooding_state_t;
+        row            : natural;
+        col            : natural
+    ) return flooding_state_t is
+        variable new_state : flooding_state_t(
+        flooding_map(heightmap'range(1), heightmap'range(2))
+        );
+    begin
+        new_state := flooding_state;
+
+        -- Flood current position
+        new_state.flooding_map(row, col) := true;
+        new_state.flooding_size          := new_state.flooding_size + 1;
+
+        -- Check and flood adjacent positions
+        if row > 0 then
+            -- Check UP
+            if not new_state.flooding_map(row - 1, col) and heightmap(row - 1, col) /= 9 then
+                -- Continue flooding as position is not yet flooded and does not contain a 9
+                new_state := perform_flooding(heightmap, new_state, row - 1, col);
+            end if;
+        end if;
+        if col > 0 then
+            -- Check LEFT
+            if not new_state.flooding_map(row, col - 1) and heightmap(row, col - 1) /= 9 then
+                -- Continue flooding as position is not yet flooded and does not contain a 9
+                new_state := perform_flooding(heightmap, new_state, row, col - 1);
+            end if;
+        end if;
+        if col < heightmap'length(2) - 1 then
+            -- Check RIGHT
+            if not new_state.flooding_map(row, col + 1) and heightmap(row, col + 1) /= 9 then
+                -- Continue flooding as position is not yet flooded and does not contain a 9
+                new_state := perform_flooding(heightmap, new_state, row, col + 1);
+            end if;
+        end if;
+        if row < heightmap'length(1) - 1 then
+            -- Check DOWN
+            if not new_state.flooding_map(row + 1, col) and heightmap(row + 1, col) /= 9 then
+                -- Continue flooding as position is not yet flooded and does not contain a 9
+                new_state := perform_flooding(heightmap, new_state, row + 1, col);
+            end if;
+        end if;
+
+        return new_state;
+    end function;
+
+    -- Calculate size of basin around given low point
+    function calculate_basin_size(
+        heightmap : heightmap_t;
+        row       : natural;
+        col       : natural
+    ) return natural is
+        variable flooding_state : flooding_state_t(
+        flooding_map(heightmap'range(1), heightmap'range(2))
+        );
+    begin
+        -- Run flooding algorithm on low point and return resulting flooding size
+        flooding_state := perform_flooding(heightmap, flooding_state, row, col);
+        return flooding_state.flooding_size;
+    end function;
+
+    -- Part Two: Calculate product of the sizes of the three largest basins
+    function calculate_basin_size_product(
+        heightmap : heightmap_t
+    ) return natural is
+        variable basin_size     : natural;
+        variable largest_basins : integer_vector(0 to 2);
+        variable smallest_index : natural;
+    begin
+        for ROW in heightmap'range(1) loop
+            for COL in heightmap'range(2) loop
+                if is_low_point(heightmap, ROW, COL) then
+                    -- Determine basin size for this low point
+                    basin_size := calculate_basin_size(heightmap, ROW, COL);
+
+                    -- Find index of smallest basin in vector
+                    smallest_index := 0;
+                    for I in largest_basins'range loop
+                        if largest_basins(I) < largest_basins(smallest_index) then
+                            smallest_index := I;
+                        end if;
+                    end loop;
+                    -- Update value in vector if basin is larger
+                    if basin_size > largest_basins(smallest_index) then
+                        largest_basins(smallest_index) := basin_size;
+                    end if;
+                end if;
+            end loop;
+        end loop;
+        -- Return product of largest three basins
+        return largest_basins(0) * largest_basins(1) * largest_basins(2);
+    end function;
+
 begin
 
     process
@@ -119,6 +223,14 @@ begin
         assert result = 423;
 
         report "*** Part Two ***";
+
+        result := calculate_basin_size_product(EXAMPLE_HEIGHTMAP);
+        report "Basin size product of example input: " & integer'image(result);
+        assert result = 1134;
+
+        result := calculate_basin_size_product(heightmap_ptr.all);
+        report "Basin size product of input file: " & integer'image(result);
+        assert result = 1198704;
 
         deallocate(heightmap_ptr);
         wait;
